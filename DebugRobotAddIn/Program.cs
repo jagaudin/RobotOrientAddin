ba3160cc5b17a661177eb65a27da80b5186b8789
Jacques Gaudin <jagaudin@gmail.com>
2019-07-05 17:45:21 +0100

    added a debug program

---
+++ DebugRobotAddIn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobotOrientAddIn;
+using RobotOM;
+
+namespace DebugRobotAddIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var add_in = new RobotOrientAddIn.RobotOrientAddIn();
+        }
+    }
+}
